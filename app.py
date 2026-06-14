0149577154a2bc57fd4ad43175dc203337674a5f
ng-srivastava7201 <nityagsrivastava@gmail.com>
2026-06-15 00:19:26 +0530

    creating database connection

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-  return "Notes"
+  return render_template('index.html')
+
+with app.app_context():
+  db.create_all()
+  
 if __name__ == '__main__':
   app.run(debug=True)
